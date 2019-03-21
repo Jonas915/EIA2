bd1da4fa03832c1704bc4be8db9cf5366586156a
Jonas Meujen <jonas.meujen@hs-furtwangen.de>
2019-03-21 14:53:27 +0100

    update2

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -5,7 +5,6 @@ Matrikel: 260231
 Datum: 22.03.2019
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 **/
-
 var Aufgabe0;
 (function (Aufgabe0) {
     function box() {
@@ -13,7 +12,7 @@ var Aufgabe0;
         var node = document.getElementById("content");
         node.innerHTML += "Hey ";
         node.innerHTML += name;
-        node.innerHTML += " ,wie geht es dir?";
+        node.innerHTML += " ,wie geht es dir";
         console.log("Hey", name, ",wie geht es dir");
     }
     document.addEventListener('DOMContentLoaded', box);
