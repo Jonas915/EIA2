5ec63940d2443df3514e09ac176166f35e61b050
Jonas Meujen <jonas.meujen@hs-furtwangen.de>
2019-04-25 11:50:04 +0200

    Aufgabe 5

---
+++ Aufgabe5/css_ts_js/FunktionenVerkaeufer.js
@@ -0,0 +1,15 @@
+/* Aufgabe: 5
+Name: Jonas Meujen
+Matrikel: 260231
+Datum: 28.04.2019
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
+var Aufgabe5;
+(function (Aufgabe5) {
+    Aufgabe5.formOptionen = [{ name: "Becher", preis: 2 }, { name: "Waffel", preis: 2 }];
+    Aufgabe5.eissorte1 = [{ name: "keine ausgewählt", preis: 0 }, { name: "Vanille", preis: 2 }, { name: "Schokolade", preis: 2 }, { name: "Erdbeere", preis: 2 }, { name: "Apfel", preis: 2 }];
+    Aufgabe5.eissorte2 = [{ name: "keine ausgewählt", preis: 0 }, { name: "Vanille", preis: 2 }, { name: "Schokolade", preis: 2 }, { name: "Erdbeere", preis: 2 }, { name: "Apfel", preis: 2 }];
+    Aufgabe5.eissorte3 = [{ name: "keine ausgewählt", preis: 0 }, { name: "Vanille", preis: 2 }, { name: "Schokolade", preis: 2 }, { name: "Erdbeere", preis: 2 }, { name: "Apfel", preis: 2 }];
+    Aufgabe5.eissorte4 = [{ name: "keine ausgewählt", preis: 0 }, { name: "Vanille", preis: 2 }, { name: "Schokolade", preis: 2 }, { name: "Erdbeere", preis: 2 }, { name: "Apfel", preis: 2 }];
+    Aufgabe5.versandoptionen = [{ name: "keine ausgewählt", preis: 0 }, { name: "DHL", preis: 4 }, { name: "DPD", preis: 5 }, { name: "Brieftaube", preis: 10 }];
+})(Aufgabe5 || (Aufgabe5 = {}));
+//# sourceMappingURL=FunktionenVerkaeufer.js.map
---
+++ Aufgabe5/css_ts_js/FunktionenVerkaeufer.ts
@@ -0,0 +1,20 @@
+/* Aufgabe: 5
+Name: Jonas Meujen
+Matrikel: 260231
+Datum: 28.04.2019 
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
+
+namespace Aufgabe5 {
+
+    export interface products{
+        name:string,
+        preis: number,
+    }
+
+    export let formOptionen: products[] = [{ name: "Becher", preis: 2}, { name: "Waffel", preis: 2}];
+    export let eissorte1: products[] = [{name:"keine ausgewählt", preis: 0}, { name: "Vanille", preis: 2}, { name: "Schokolade", preis: 2}, { name: "Erdbeere", preis: 2 }, { name: "Apfel", preis: 2 }];
+    export let eissorte2: products[] = [{name:"keine ausgewählt", preis: 0}, { name: "Vanille", preis: 2}, { name: "Schokolade", preis: 2}, { name: "Erdbeere", preis: 2 }, { name: "Apfel", preis: 2 }];
+    export let eissorte3: products[] = [{name:"keine ausgewählt", preis: 0}, { name: "Vanille", preis: 2}, { name: "Schokolade", preis: 2}, { name: "Erdbeere", preis: 2 }, { name: "Apfel", preis: 2 }];
+    export let eissorte4: products[] = [{name:"keine ausgewählt", preis: 0}, { name: "Vanille", preis: 2}, { name: "Schokolade", preis: 2}, { name: "Erdbeere", preis: 2 }, { name: "Apfel", preis: 2 }];
+    export let versandoptionen: products[] = [{name:"keine ausgewählt", preis:0}, { name: "DHL", preis: 4 }, { name: "DPD", preis: 5 }, { name: "Brieftaube", preis: 10 }];
+}
