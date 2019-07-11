b350b527e0432304d6a4ab31a9966596881ad4fd
Jonas Meujen <jonas.meujen@hs-furtwangen.de>
2019-07-11 16:51:16 +0200

    ...

--- Abschlussaufgabe/server/Database.js
+++ Abschlussaufgabe/server/Database.js
@@ -13,8 +13,8 @@ let abschlussaufgabe;
 // running on heroku?
 if (process.env.NODE_ENV == "production") {
     //    databaseURL = "mongodb://username:password@hostname:port/database";
-    databaseURL = "mongodb://FlappyFishGame:FlappyFishGame@cluster0-oy8uv.mongodb.net/Fleischfackeln24";
-    databaseName = "Fleischfackeln24";
+    databaseURL = "mongodb+srv://anyUser:savePasswort@clusterfuwa-pmutc.mongodb.net/fThisFingF";
+    databaseName = "fThisFingF";
 }
 // try to connect to database, then activate callback "handleConnect" 
 Mongo.MongoClient.connect(databaseURL, handleConnect);
